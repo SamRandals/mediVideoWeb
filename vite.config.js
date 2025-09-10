e86cbf6cc92b30e36d06637bdf02a3579f72108f
SamRandals <samnanezedu@gmail.com>
2025-09-10 18:16:48 -0500

    small changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Nombre del repositorio (ajústalo al tuyo exacto en GitHub)
+const repoName = "mediVideoWeb"
+
+// Detectar si estamos en GitHub Pages (usando variable de entorno)
+const isGithubPages = process.env.GITHUB_PAGES === "true"
+
 export default defineConfig({
   plugins: [react()],
-   base: "./",
+  base: isGithubPages ? `/${repoName}/` : "./",
 })
